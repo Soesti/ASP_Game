c02388c0263271fce9ff15d462a485e89722dc5a
Julia <jungmannjulia@gmail.com>
2016-03-18 15:14:15 +0100

    Question draft added

---
+++ ASP_Game/src/hs/ss16/asp/QuestionHandler.java
@@ -0,0 +1,16 @@
+package hs.ss16.asp;
+
+import java.util.List;
+
+public class QuestionHandler {
+	List<Question> questions;
+
+	public QuestionHandler(){
+		//read Questions from xml
+	}
+	
+	public List<Question> getQuestions(){
+		return this.questions;
+	}
+	
+}
