4ac89d17fef54e64db514e3dbd855c8cd3055c18
Soesti <Max@Max-PC>
2016-03-19 16:32:34 +0100

    Player have now lifes and getter and setter for them

--- ASP_Game/src/hs/ss16/asp/Player.java
+++ ASP_Game/src/hs/ss16/asp/Player.java
@@ -6,12 +6,14 @@ import javax.swing.ImageIcon;
 public class Player extends Sprite {
 	
 	private Direction direction = Direction.Top;
-	private int player_speed = 5;
+	private int lives;
 
 	public Player(int xPosition, int yPosition) {
 		super(xPosition, yPosition);
 		ImageIcon ii = new ImageIcon("img/player.png");
 		image = ii.getImage();
+		speed = 5;
+		lives = 3;
 	}
 	
 	public void setDirection(Direction dir){
@@ -21,19 +23,27 @@ public class Player extends Sprite {
 	@Override
 	public void calculatePosition(){
 		if(direction == Direction.Left){
-			if(xPosition - player_speed > 0){
-				xPosition = xPosition - player_speed;
+			if(xPosition - speed > 0){
+				xPosition = xPosition - speed;
 			}
 		}
 		if(direction == Direction.Right){
-			if(xPosition + player_speed < 930){
-				xPosition = xPosition + player_speed;
+			if(xPosition + speed < 930){
+				xPosition = xPosition + speed;
 			}
 		}
 	}
 	
 	public Direction getDirection(){
 		return direction;
+	}
+	
+	public int getCurrentLives(){
+		return lives;
+	}
+	
+	public void decrementLive(){
+		lives--;
 	}
 
 }
