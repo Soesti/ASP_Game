4152ebb983e9d5961f96795117e881534ae183ea
Julia <julia@lule.de>
2016-03-21 23:17:29 +0100

    added reading questions from XML

---
+++ ASP_Game/src/hs/ss16/asp/QuestionGUI.java
@@ -0,0 +1,44 @@
+package hs.ss16.asp;
+
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class QuestionGUI extends JPanel {
+	
+	private static final long serialVersionUID = -4767294290332006077L;
+	static final int G_Width = 600;
+	static final int G_Height = 300;
+	
+	JLabel questionLabel, answer1Label, answer2Label, answer3Label, answer4Label;
+	
+	QuestionHandler questionHandler;
+	
+	public QuestionGUI(Board board){
+		setSize(G_Width, G_Height);
+		setLayout(new GridLayout(5,1));
+		
+		questionHandler = new QuestionHandler();
+		
+		questionLabel = new JLabel();
+		answer1Label = new JLabel();
+		answer2Label = new JLabel();
+		answer3Label = new JLabel();
+		answer4Label = new JLabel();
+		
+	}
+	
+	public void createNewQuestion(){
+		Question newQuestion = questionHandler.getQuestion();
+		
+		questionLabel.setText(newQuestion.getQuestion());
+		answer1Label.setText(newQuestion.getAnswers().get(0).getAnswerString());
+		answer1Label.setText(newQuestion.getAnswers().get(1).getAnswerString());
+		answer1Label.setText(newQuestion.getAnswers().get(2).getAnswerString());
+		answer1Label.setText(newQuestion.getAnswers().get(3).getAnswerString());
+		
+		setVisible(true);
+	}
+
+}
--- ASP_Game/src/hs/ss16/asp/QuestionHandler.java
+++ ASP_Game/src/hs/ss16/asp/QuestionHandler.java
@@ -1,17 +1,73 @@
 package hs.ss16.asp;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class QuestionHandler {
 	List<Question> questions;
 
-	public QuestionHandler(){
-		//read Questions from xml
+	public QuestionHandler() {
 	}
-	
-	public Question getQuestions(){
-		int randomNumber = (int)Math.random()*questions.size();
-		return questions.get(randomNumber);		
+
+	public Question getQuestion() {
+		if (this.questions == null) {
+			getQuestionsFromXML();
+		}
+		int randomNumber = (int) Math.random() * questions.size();
+		return questions.get(randomNumber);
 	}
-	
+
+	private void getQuestionsFromXML() {
+		this.questions = new ArrayList<Question>();
+		try {
+			File questionFile = new File("xml/questions.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(questionFile);
+
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("question");
+
+			for (int i = 0; i < nList.getLength(); i++) {
+				List<Answer> answers = new ArrayList<Answer>();
+				Node questionNode = nList.item(i);
+				if (questionNode.getNodeType() == Node.ELEMENT_NODE) {
+					boolean right = false;
+					Element questionElement = (Element) nList.item(i);
+					String questionText = questionElement.getAttribute("text");
+
+					NodeList answerNodes = questionElement.getElementsByTagName("answer");
+					for (int j = 0; j < answerNodes.getLength(); j++) {
+						Node answerNode = answerNodes.item(j);
+						if (answerNode.getNodeType() == Node.ELEMENT_NODE) {
+							Element answerElement = (Element) answerNode;
+							String answerText = answerElement.getAttribute("text");
+
+							if (answerElement.hasAttribute("right")) {
+								right = true;
+							}
+							answers.add(new Answer(answerText, right));
+						}
+					}
+					Question question = new Question(answers, questionText);
+					this.questions.add(question);
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+		}
+		;
+	}
 }
