f09df9457ad1d530cdf4ff2233b9f19235a490db
Soesti <Max@Max-PC>
2016-03-25 16:27:50 +0100

    Fix highscore override bug

--- ASP_Game/src/hs/ss16/asp/HighscorePanel.java
+++ ASP_Game/src/hs/ss16/asp/HighscorePanel.java
@@ -190,8 +190,9 @@ public class HighscorePanel extends JPanel {
 		int scoreIndex = scoreTimes.length - 1;
 		
 		
-		while(scoreIndex > insertIndex && scoreIndex > 0) {
+		while(scoreIndex >= insertIndex && scoreIndex > 0) {
 			scoreTimes[scoreIndex] = scoreTimes[scoreIndex - 1];
+			scoreNames[scoreIndex] = scoreNames[scoreIndex -1];
 			
 			scoreIndex--;
 		}
